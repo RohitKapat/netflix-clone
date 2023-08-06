d364cd40c6b52e3a8e68817514914f178186c535
RohitKapat <101118338+RohitKapat@users.noreply.github.com>
2023-08-06 18:36:55 +0530

    day 1 complete

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
-}
-
+    images: {
+      remotePatterns: [
+        {
+          hostname: 'image.tmdb.org',
+        },
+      ],
+    },
+  }
+ 
 module.exports = nextConfig
